90b36a54b68de050d2601e433b91f02dab1b2bed
sotaoverride <aatrapps@gmail.com>
2021-06-08 10:19:36 -0400

    add command line arguments to bash script

--- ot3Image.sh
+++ ot3Image.sh
@@ -1,9 +1,27 @@
-#! /bin/sh
+#! /bin/bash
+branch=  
+while getopts ":b:" opt; do
+  case $opt in
+    b)
+      branch=$OPTARG >&2
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+shift $((OPTIND - 1))
+
 mkdir docker-test
 cd docker-test
 mkdir -p oe-core/build
 #change branch to main eventually, or pass as cmdline args
-curl 'https://raw.githubusercontent.com/Opentrons/meta-opentrons/docker-ot3/Dockerfile' > DockerFile
-curl 'https://raw.githubusercontent.com/Opentrons/meta-opentrons/docker-ot3/start.sh' > start.sh
+curl 'https://raw.githubusercontent.com/Opentrons/meta-opentrons/${branch}/Dockerfile' > DockerFile
+curl 'https://raw.githubusercontent.com/Opentrons/meta-opentrons/${branch}/start.sh' > start.sh
 docker run -it --rm -v $PWD/oe-core/build:/home/ot3/oe-core/build/deploy ot3-image:latest
 
