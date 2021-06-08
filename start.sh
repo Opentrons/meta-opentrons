1412c85f51047e6bcbf0a35858f5f79ac27b4254
sotaoverride <aatrapps@gmail.com>
2021-06-08 14:16:29 -0400

    start.sh use-head-next

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@ rm -rf /home/ot3/oe-core/build/deploy
 source /home/ot3/oe-core/export
 cd /home/ot3/oe-core/build
 echo 'ACCEPT_FSL_EULA = "1"' >> /home/ot3/oe-core/build/conf/local.conf
+echo 'MACHINEOVERRIDES =. “use-head-next:”' >> /home/ot3/oe-core/build/conf/local.conf
 sed -i '/#MACHINE ?= "verdin-imx8mm"/c\MACHINE ?= "verdin-imx8mm"' /home/ot3/oe-core/build/conf/local.conf
 sed -i '/MACHINE ?= "colibri-imx6"/c\#MACHINE ?= "colbri-imx6"' /home/ot3/oe-core/build/conf/local.conf
 bitbake tdx-reference-minimal-image
