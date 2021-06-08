931b8520d4a7130c8075aee196e18a1bcb998335
sotaoverride <aatrapps@gmail.com>
2021-06-08 13:54:52 -0400

    double quotes vs single for string concat

--- ot3Image.sh
+++ ot3Image.sh
@@ -4,6 +4,7 @@ while getopts ":b:" opt; do
   case $opt in
     b)
       branch=$OPTARG >&2
+      echo $OPTARG
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
@@ -17,14 +18,13 @@ while getopts ":b:" opt; do
 done
 shift $((OPTIND - 1))
 
+
 mkdir -p docker
 cd docker
 mkdir -p oe-core/build
 
-curl 'https://raw.githubusercontent.com/Opentrons/meta-opentrons/${branch}/Dockerfile' > DockerFile
-curl 'https://raw.githubusercontent.com/Opentrons/meta-opentrons/${branch}/start.sh' > start.sh
+echo $branch
+curl "https://raw.githubusercontent.com/Opentrons/meta-opentrons/${branch}/Dockerfile" > Dockerfile
+curl "https://raw.githubusercontent.com/Opentrons/meta-opentrons/${branch}/start.sh" > start.sh
 
-docker build --no-cache --build-arg "host_uid=$(id -u)"   --build-arg "host_gid=$(id -g)" --tag "ot3-image:latest" .
-
-docker run -it --rm -v $PWD/oe-core/build:/home/ot3/oe-core/build/deploy ot3-image:latest
 
