bf75fbbb111b66cfef3c39c0940fe896d619b2a5
sotaoverride <aatrapps@gmail.com>
2021-06-09 08:59:48 -0400

    revert change in start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #! /bin/sh
-rm -rf /home/ot3/oe-core/build/tmp
 rm -f /home/ot3/oe-core/build/tmp-glibc
 rm -rf /home/ot3/oe-core/build/deploy
 source /home/ot3/oe-core/export
@@ -7,5 +6,4 @@ cd /home/ot3/oe-core/build
 echo 'ACCEPT_FSL_EULA = "1"' >> /home/ot3/oe-core/build/conf/local.conf
 sed -i '/#MACHINE ?= "verdin-imx8mm"/c\MACHINE ?= "verdin-imx8mm"' /home/ot3/oe-core/build/conf/local.conf
 sed -i '/MACHINE ?= "colibri-imx6"/c\#MACHINE ?= "colbri-imx6"' /home/ot3/oe-core/build/conf/local.conf
-sed -i '/INHERIT += "toradex-mirrors"/c\#INHERIT += "toradex-mirrors"' /home/ot3/oe-core/build/conf/local.conf
 bitbake tdx-reference-minimal-image
