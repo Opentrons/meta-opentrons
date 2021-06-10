44d48740ac0bbfd1acc50731eb4dcb5a37041b2d
sotaoverride <aatrapps@gmail.com>
2021-06-10 14:28:53 -0400

    look for mirrirs

--- start.sh
+++ start.sh
@@ -2,7 +2,10 @@
 rm -f /home/ot3/oe-core/build/tmp-glibc
 source /home/ot3/oe-core/export
 cd /home/ot3/oe-core/build
+echo 'INHERIT += "toradex-mirrors"' >> /home/ot3/oe-core/build/conf/local.conf
+echo 'MACHINEOVERRIDES =. "use-head-next:"' >> /home/ot3/oe-core/build/conf/local.conf
 echo 'ACCEPT_FSL_EULA = "1"' >> /home/ot3/oe-core/build/conf/local.conf
 sed -i '/#MACHINE ?= "verdin-imx8mm"/c\MACHINE ?= "verdin-imx8mm"' /home/ot3/oe-core/build/conf/local.conf
 sed -i '/MACHINE ?= "colibri-imx6"/c\#MACHINE ?= "colbri-imx6"' /home/ot3/oe-core/build/conf/local.conf
+bitbake -c cleanall tdx-reference-minimal-image
 bitbake tdx-reference-minimal-image
