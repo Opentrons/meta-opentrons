42d0725c582372c0906857b4694efbc3e361a389
sotaoverride <aatrapps@gmail.com>
2021-06-12 23:57:55 -0400

    full paths

--- start.sh
+++ start.sh
@@ -10,7 +10,7 @@ echo 'ACCEPT_FSL_EULA = "1"' >> /home/ot3/oe-core/build/conf/local.conf
 sed -i '/#MACHINE ?= "verdin-imx8mm"/c\MACHINE ?= "verdin-imx8mm"' /home/ot3/oe-core/build/conf/local.conf
 sed -i '/MACHINE ?= "colibri-imx6"/c\#MACHINE ?= "colbri-imx6"' /home/ot3/oe-core/build/conf/local.conf
 
-curl "https://raw.githubusercontent.com/Opentrons/meta-opentrons/${branch}/linux-toradex_5.4-2.3.x.patch" > linux-toradex_5.4-2.3.x.patch
-patch ../layers/meta-toradex-nxp/recipes-kernel/linux/linux-toradex_5.4-2.3.x.bb linux-toradex_5.4-2.3.x.patch
+curl "https://raw.githubusercontent.com/Opentrons/meta-opentrons/${branch}/linux-toradex_5.4-2.3.x.patch" > /home/ot3/oe-core/build/linux-toradex_5.4-2.3.x.patch
+patch /home/ot3/oe-core/layers/meta-toradex-nxp/recipes-kernel/linux/linux-toradex_5.4-2.3.x.bb /home/ot3/oe-core/build/linux-toradex_5.4-2.3.x.patch
 bitbake -c cleanall tdx-reference-minimal-image
 bitbake tdx-reference-minimal-image
