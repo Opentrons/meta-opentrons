be0eea7c332b64519679f2c43209d382dc0ede1f
sotaoverride <aatrapps@gmail.com>
2021-06-13 23:23:10 -0400

    clean build

--- ot3Image.sh
+++ ot3Image.sh
@@ -28,6 +28,7 @@ curl "https://raw.githubusercontent.com/Opentrons/meta-opentrons/${branch}/Docke
 curl "https://raw.githubusercontent.com/Opentrons/meta-opentrons/${branch}/start.sh" > start.sh
 curl "https://raw.githubusercontent.com/Opentrons/meta-opentrons/${branch}/linux-toradex_5.4-2.3.x.patch" > linux-toradex_5.4-2.3.x.patch
 
-docker build --no-cache --build-arg "host_uid=1000"   --build-arg "host_gid=1000" --tag "ot3-image:latest" .
+
+docker build --build-arg "host_uid=1000"   --build-arg "host_gid=1000" --tag "ot3-image:latest" .
 docker run -it --rm -v $PWD/oe-core/build:/home/ot3/oe-core/build/deploy ot3-image:latest
 
